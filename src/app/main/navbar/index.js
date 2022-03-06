df08524f18818b004699bb344b9df9b194e96e72
Tomyred <tomy.rojo.1@gmail.com>
2022-03-06 17:33:42 -0300

    added: mobile navigation

--- src/app/main/navbar/index.js
+++ src/app/main/navbar/index.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import navigationConfig from "../../config/navigationConfig";
 import resume from "../../assets/docs/resume.pdf";
+import Collapsible from "react-collapsible";
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className="navbar">
-            <div className="navbar__title">
-                <h1>{"{{Logo here}}"}</h1>
-            </div>
-            <div className="navigation">
-                {navigationConfig.map((element, i) => {
-                    return (
-                        <span key={i}>
-                            <a href={element.href}>{element.title}</a>
-                        </span>
-                    );
-                })}
-                <span>
+        <div className="navbar__container">
+            <div className="navbar">
+                <div className="navbar__title">
+                    <h1>{"{{Logo here}}"}</h1>
+                </div>
+                <div className="bw__navigation">
+                    {navigationConfig.map((element, i) => {
+                        return (
+                            <a key={i} href={element.href}>
+                                {element.title}
+                            </a>
+                        );
+                    })}
+
                     <a href={resume} download="Tomás_Rojo_resume">
                         Dowload Resume
                     </a>
-                </span>
+                </div>
+                <div className="burger" onClick={() => setOpen(!open)}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </div>
+            <div className="collapsible">
+                <Collapsible
+                    open={open}
+                    transitionTime={200}
+                    onTriggerClosing={() => setOpen(false)}
+                >
+                    {navigationConfig.map((element, i) => {
+                        return (
+                            <a
+                                key={i}
+                                className="collapse__item"
+                                href={element.href}
+                                onClick={() => setOpen(false)}
+                            >
+                                {element.title}
+                            </a>
+                        );
+                    })}
+
+                    <a
+                        className="collapse__item"
+                        href={resume}
+                        download="Tomás_Rojo_resume"
+                    >
+                        Dowload Resume
+                    </a>
+                </Collapsible>
             </div>
         </div>
     );
