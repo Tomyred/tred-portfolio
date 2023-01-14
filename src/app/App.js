5d6409f9e58902379f73db57a4c23b8eb3949195
Tomas Rojo <tomas.rojo@softtek.com>
2023-01-14 20:45:22 -0300

    actualizaciona a react 18

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,10 @@
-// import * as THREE from "three";
-
 import AboutMe from "./main/abourMe";
 import Navbar from "./main/navbar";
 import Home from "./main/home";
 import Summary from "./main/summary";
 import Contact from "./main/contact";
 import Experience from "./main/summary/experience";
+import "./sass/main.scss";
 
 function App() {
     return (
