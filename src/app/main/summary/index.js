d80369a205a33bf068cb7feae7da2c69277ce2d7
Tomas Rojo <tomas.rojo@softtek.com>
2023-01-15 16:11:39 -0300

    unused code removed

--- src/app/main/summary/index.js
+++ src/app/main/summary/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Projects from "./projects";
 import TechGrid from "./techGrid";
 
 const Summary = () => {
     return (
         <section id="summary">
             <div className="summary page-section">
-                {/* <Projects /> */}
                 <TechGrid />
             </div>
         </section>
